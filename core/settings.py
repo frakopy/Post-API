955f3f0c3f83a9c6483c1121469ba558461a9c91
frakopy <frako789@gmail.com>
2023-06-25 11:51:36 -0500

    setting media url for images upladed

--- core/settings.py
+++ core/settings.py
@@ -152,6 +152,10 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = './static/'
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / MEDIA_URL
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
